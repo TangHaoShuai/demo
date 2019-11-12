e11d4fa75c616896c0c44dcbb8229032009891e7
TangHaoShuai <1648375651@qq.com>
2019-11-12 14:28:50 +0800

    数据库的配置 jap user 类的创建

--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -1,5 +1,28 @@
 package com.example.demo.model;
 
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "User")
+@GenericGenerator(name = "jpa-uuid", strategy = "uuid")
 public class User {
+    @Id
+    @GeneratedValue(generator = "jpa-uuid")
+    @Column(length = 32)
+    private String uid;
+
+    @Column(name="userName",length = 200)
+    private String userName;
+
+    @Column(name="passWord",length = 200)
+    private String passWord;
+
+    @Column(name="sex")
+    private Integer sex;  ///1为男性 2为女性
+
+    @Column(name = "phone")
+    private String phone;
 
 }
